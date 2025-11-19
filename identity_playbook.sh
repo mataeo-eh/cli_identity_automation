e9f0e6599ffc28e9849b1499a7952a36fe3bffe7
Mataeo <mataeo.eh@gmail.com>
2025-11-19 11:36:26 -0600

    added the necesary empty files as place holders to be filled in throughout the assignment.

---
+++ identity_playbook.sh
@@ -0,0 +1,6 @@
+# Log into azure cli using az login and following the prompt that appears
+# Once logged in, select the subscription to log into, for this case [1] the student subscription
+# az account list -o table will show which subscriptions are available for your account
+    # The generated table also shows which subscription is the default in the IsDefault column
+    # My student subscription is the defualt subscription, and the only one available to me
+
